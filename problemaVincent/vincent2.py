fc30ae57046692e640a568f58a156f80f4fc138c
root <andresmauriciogomezr@gmail.com>
2017-02-24 09:28:45 -0500

    Funcionando todo

--- problemaVincent/vincent2.py
+++ problemaVincent/vincent2.py
@@ -3,7 +3,7 @@
 
 class Vincent:
 
-	cantidadInicial = 50000
+	cantidadInicial = 500000
 	porcentajeInteres = 10
 	cantidadDias = 30
 
@@ -13,6 +13,14 @@ class Vincent:
 
 	cantidadDiasSimulacion = 1080 # determina la cantidad de dìas para las que se harà la simulación 
 
+	prestamoMinimo = 10000 # Cantidad minima para hacer un presamo
+
+	alcanzaMillon = '' # Guardarà la cantidad de días en que se alcanza el millon
+	objetivo = 1000000 # Guarda la cantidad objetivo 
+
+	cantidadDiasSinPrestamo = 0
+
+
 	def prestar(self):
 
 		prestamo = Prestamo(self.cantidadInicial, self.porcentajeInteres, self.cantidadDias) #es el prestamo inicial
@@ -20,7 +28,8 @@ class Vincent:
 		#print "Prestamo 0 Se presto " +  str(self.cantidadInicial )+ "	Ahora hay "	+  str(self.cantidadPrestada)
 
 		i = 1
-		acumuladoCobro = 0
+		acumuladoCobro = 0 # Se usa para acumular el cobro diario hasta completar 10000
+
 		while i <= self.cantidadDiasSimulacion:
 			
 			cobroDiario = 0 # Guardará el total que se recoge cada dìa
@@ -29,10 +38,10 @@ class Vincent:
 				#print "prestamo de " + str(prestamo.valorCuotaDiaria)
 				cobroDiario = cobroDiario + prestamo.cobrar()				
 
-				if prestamo.cantidadDias == 0: # ya se pagó todo el prestamo
+				if prestamo.cantidadDias == self.cantidadDiasSinPrestamo: # ya se pagó todo el prestamo
 					self.listaPrestamos.remove(prestamo)
 
-			if cobroDiario >= 10000:
+			if cobroDiario >= self.prestamoMinimo: # El acumulado es suficiente para el prestamo
 				nuevoPrestamo = Prestamo(cobroDiario, self.porcentajeInteres, self.cantidadDias) # Nuevo prestamo con lo recogido durante el día
 				self.listaPrestamos.append(nuevoPrestamo)	
 
@@ -42,7 +51,7 @@ class Vincent:
 				acumuladoCobro = acumuladoCobro + cobroDiario
 
 
-			if acumuladoCobro >= 10000:
+			if acumuladoCobro >= self.prestamoMinimo: # El acumulado es suficiente para el prestamo
 				nuevoPrestamo = Prestamo(acumuladoCobro, self.porcentajeInteres, self.cantidadDias) # Nuevo prestamo con lo recogido durante el día
 				self.listaPrestamos.append(nuevoPrestamo)	
 
@@ -51,13 +60,15 @@ class Vincent:
 
 				acumuladoCobro = 0
 			
-			if self.cantidadPrestada >= 1000000 and self.cantidadPrestada < 1010000:
-				print "se alcanzó el millòn a los " + str(i) + " dìas"
+			if self.cantidadPrestada >= self.objetivo and self.cantidadPrestada < self.objetivo + ((self.cantidadInicial * 10) /100):
+			#if self.cantidadPrestada >= self.objetivo and self.cantidadPrestada < self.objetivo + ((self.objetivo * 0.5) /100):
+				self.alcanzaMillon = i
 
 
 			i = i + 1
 
 		print "En tres años se logró acumular la cantidad de: " + str(self.cantidadPrestada)
+		print "se alcanzó el objetivo a los " + str(self.alcanzaMillon) + " dìas"
 
 
 
