48e78b22e2a1ea287f46ab6bd80a1c1c7c9fcae1
root <root@localhost.localdomain>
2017-02-23 21:57:37 -0500

    fnciona la segunda parte

--- problemaVincent/vincent2.py
+++ problemaVincent/vincent2.py
@@ -3,7 +3,7 @@
 
 class Vincent:
 
-	cantidadInicial = 500000 
+	cantidadInicial = 50000
 	porcentajeInteres = 10
 	cantidadDias = 30
 
@@ -20,6 +20,7 @@ class Vincent:
 		#print "Prestamo 0 Se presto " +  str(self.cantidadInicial )+ "	Ahora hay "	+  str(self.cantidadPrestada)
 
 		i = 1
+		acumuladoCobro = 0
 		while i <= self.cantidadDiasSimulacion:
 			
 			cobroDiario = 0 # Guardará el total que se recoge cada dìa
@@ -31,15 +32,28 @@ class Vincent:
 				if prestamo.cantidadDias == 0: # ya se pagó todo el prestamo
 					self.listaPrestamos.remove(prestamo)
 
+			if cobroDiario >= 10000:
+				nuevoPrestamo = Prestamo(cobroDiario, self.porcentajeInteres, self.cantidadDias) # Nuevo prestamo con lo recogido durante el día
+				self.listaPrestamos.append(nuevoPrestamo)	
 
-			self.cantidadPrestada = self.cantidadPrestada + cobroDiario
+				self.cantidadPrestada = self.cantidadPrestada + cobroDiario
+				print "Prestamo " + str(i) + " Se presto " +  str(cobroDiario )+ "	Ahora hay "	+  str(self.cantidadPrestada)
+			else:
+				acumuladoCobro = acumuladoCobro + cobroDiario
 
-			nuevoPrestamo = Prestamo(cobroDiario, self.porcentajeInteres, self.cantidadDias) # Nuevo prestamo con lo recogido durante el día
-			self.listaPrestamos.append(nuevoPrestamo)	
+
+			if acumuladoCobro >= 10000:
+				nuevoPrestamo = Prestamo(acumuladoCobro, self.porcentajeInteres, self.cantidadDias) # Nuevo prestamo con lo recogido durante el día
+				self.listaPrestamos.append(nuevoPrestamo)	
+
+				self.cantidadPrestada = self.cantidadPrestada + acumuladoCobro
+				print "Prestamo " + str(i) + " Se presto " +  str(acumuladoCobro )+ "	Ahora hay "	+  str(self.cantidadPrestada)
+
+				acumuladoCobro = 0
 			
 			if self.cantidadPrestada >= 1000000 and self.cantidadPrestada < 1010000:
 				print "se alcanzó el millòn a los " + str(i) + " dìas"
-			print "Prestamo " + str(i) + " Se presto " +  str(cobroDiario )+ "	Ahora hay "	+  str(self.cantidadPrestada)
+
 
 			i = i + 1
 
@@ -65,11 +79,9 @@ class Prestamo:
 		return self.valorCuotaDiaria
 
 	def ajustarValorCuota(self):
-		print "valor antes " + str(self.valorCuotaDiaria) 
 		residuo = self.valorCuotaDiaria % 50
 		faltante = 50 - residuo
 		self.valorCuotaDiaria = self.valorCuotaDiaria +  faltante
-		print "valor despues " + str(self.valorCuotaDiaria) 
 
 
 
